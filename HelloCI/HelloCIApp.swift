46209c118e8b6fff44a1575bcc8794c2ac3c039c
Avinash Ranjan <avinash@appscelestial.com>
2025-05-18 13:57:19 +0530

    Initial Commit

---
+++ HelloCI/HelloCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCIApp.swift
+//  HelloCI
+//
+//  Created by Avinash Pandey on 18/05/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloCITests/HelloCITests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCITests.swift
+//  HelloCITests
+//
+//  Created by Avinash Pandey on 18/05/25.
+//
+
+import Testing
+@testable import HelloCI
+
+struct HelloCITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
